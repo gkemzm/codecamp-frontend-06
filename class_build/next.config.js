255304093d1783de1987bc747d210568cf34d0d2
gkemzm <gkemzm@gmail.com>
2022-05-05 15:25:10 +0900

    8주4일차

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "pbs1014",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }), // html file을 만들어 준다. 정적파일을 미리 만들어줌
 };
 
 module.exports = nextConfig;
